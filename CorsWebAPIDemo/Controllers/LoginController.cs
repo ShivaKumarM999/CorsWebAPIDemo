e18096431dca4a7e7c3a7c38efa28d864f96c241
ShivaKumarM999 <81612983+ShivaKumarM999@users.noreply.github.com>
2021-04-07 17:02:27 +0530

    Add project files.

---
+++ CorsWebAPIDemo/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CorsWebAPIDemo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddLogin()
+        {
+            return View();
+        }
+        public ActionResult DeleteLogin()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateLogin()
+        {
+            return View();
+        }
+        public ActionResult GetLoginById()
+        {
+            return View();
+        }
+    }
+}
---
+++ CorsWebAPIDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CorsWebAPIDemo.MvcApplication" Language="C#" %>
